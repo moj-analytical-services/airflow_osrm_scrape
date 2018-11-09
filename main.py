feb56114606bdc385857e802752eed58e4fd4975
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-11-09 08:48:49 +0000

    update for airflow

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ BLOCK_SIZE = 200
 if __name__ == "__main__":
     get_lsoas()
     get_msoas()
-    scrape(BLOCK_SIZE, "all_lsoas_with_coords.csv", "s3://alpha-everyone/travel_time_2/lsoas", "lsoa11cd", "lsoa")
-    scrape(BLOCK_SIZE, "all_msoas_with_coords.csv", "s3://alpha-everyone/travel_time_2/msoas", "msoa11cd", "msoa")
+    scrape(BLOCK_SIZE, "all_lsoas_with_coords.csv", "s3://mojap-raw-hist/open_data/osrm/geographytype=lsoa", "lsoa11cd", "lsoa")
+    scrape(BLOCK_SIZE, "all_msoas_with_coords.csv", "s3://mojap-raw-hist/open_data/osrm/geographytype=msoa", "msoa11cd", "msoa")
