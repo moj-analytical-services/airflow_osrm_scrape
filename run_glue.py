273e0b0a51be11e735b47a7a19d8cffa60620e05
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-11-17 12:23:21 +0000

    Improve glue job

--- run_glue.py
+++ run_glue.py
@@ -1,7 +1,13 @@
 from etl_manager.etl import GlueJob
 
-g = GlueJob('gluejob/', bucket = 'alpha-mojap-curated-open-data', job_role = 'airflow_osrm_scraper', job_arguments={'--test_arg': 'this is a test', '--enable-metrics': ''})
+job = GlueJob('gluejob/', bucket = 'alpha-mojap-curated-open-data', job_role = 'airflow_osrm_scraper', job_arguments={'--test_arg': 'this is a test', '--enable-metrics': ''})
 
-g.allocated_capacity = 4
+job.allocated_capacity = 4
 
-g.run_job()
+
+try:
+    job.run_job()
+    job.wait_for_completion()
+finally:
+    job.cleanup()
+
