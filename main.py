4c1edf02b9906f4bebd27f0cec9dcce1d903df50
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-11-17 12:48:25 +0000

    full datasets

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import pandas as pd
 BLOCK_SIZE = 200
 
 if __name__ == "__main__":
-    lsoas = get_lsoas().head(100)
-    msoas = get_msoas().head(100)
-    courts = pd.read_csv("courts_list.csv").head(100)
+    lsoas = get_lsoas()
+    msoas = get_msoas()
+    courts = pd.read_csv("courts_list.csv")
     points = pd.concat([lsoas,msoas, courts])
     scrape(BLOCK_SIZE, points, "s3://mojap-raw-hist/open_data/osrm/combinations=courts_lsoas_msoas")
