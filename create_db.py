4dc0ba979ad0e570a379e30f46d23f74208294db
Karik Isichei <karik.isichei@justice.gsi.gov.uk>
2019-01-17 15:49:26 +0000

    updating create_db script as the meta data no longer aligned to data. Adding issue as the column names have typos - really not urgent though

--- create_db.py
+++ create_db.py
@@ -1,18 +1,22 @@
 from etl_manager import meta
 
-
-
 db = meta.DatabaseMeta(name="open_data", bucket="alpha-mojap-curated-open-data")
 table = meta.TableMeta(name="lsoa_travel_time", location = "osrm",data_format='parquet')
 
-
-table.add_column(name="from_lsoa", type="character", description="from_lsoa")
-table.add_column(name="to_lsoa", type="character", description="to_lsoa")
-table.add_column(name="duration_seconds", type="double", description="duration_seconds")
+table.add_column(name="source_geography_id", type="character", description="from_lsoa")
+table.add_column(name="destination_geography_id", type="character", description="to_lsoa")
+table.add_column(name="duration_sections", type="double", description="duration_seconds")
 table.add_column(name="distance_meters", type="double", description="distance_meters")
+table.add_column(name="__index_level_0__", type="long", description="ignore this")
 
-# Add to existing db rather than create new
 
+# Add partitions
+table.add_column(name="source_geography_type", type="character", description="source_geography_type")
+table.add_column(name="destination_geography_type", type="character", description="destination_geography_type")
+
+table.partitions = ['source_geography_type', 'destination_geography_type']
+
+# Add table to existing db rather than a whole new db
 table.database = db
 td = table.glue_table_definition()
 
@@ -26,4 +30,3 @@ except glue_client.exceptions.EntityNotFoundException:
     pass
 
 glue_client.create_table(DatabaseName="open_data", TableInput=td)
-
